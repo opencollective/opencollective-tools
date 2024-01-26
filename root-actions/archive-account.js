3184c3213806dc721ba90f8b9e678f9928a3c6d3
François Hodierne <francois@hodierne.net>
2024-01-26 08:44:57 +0100

    move to personal token

--- root-actions/archive-account.js
+++ root-actions/archive-account.js
@@ -4,9 +4,13 @@ const { Command } = require('commander');
 
 const { request, gql } = require('graphql-request');
 
-const endpoint = `${process.env.API_URL}/graphql/v2/${process.env.API_KEY}`;
+const endpoint = process.env.PERSONAL_TOKEN
+  ? `${process.env.API_URL}/graphql?personalToken=${process.env.PERSONAL_TOKEN}`
+  : `${process.env.API_URL}/graphql/${process.env.API_KEY}`;
 
-const endpointV1 = `${process.env.API_URL}/graphql/v1/${process.env.API_KEY}`;
+const endpointV1 = process.env.PERSONAL_TOKEN
+  ? `${process.env.API_URL}/graphql/v1?personalToken=${process.env.PERSONAL_TOKEN}`
+  : `${process.env.API_URL}/graphql/v1/${process.env.API_KEY}`;
 
 const accountQuery = gql`
   query Account($slug: String) {
